28ebccb128762a081b75f4bbc6560c0f450da198
Isabelabigdataprofissional <isabelaprofissional.bigdata@gmail.com>
2025-05-08 17:22:20 -0300

    learn(ap11): adiciono novos exercicios

--- Sprocedure.sql
+++ Sprocedure.sql
@@ -268,10 +268,31 @@ $$
 
 CALL sp_pedidos_do_cliente (1);
 
+--=============================================================================================================
+-- 3 Reescreva o exercício 1.2 de modo que o total de pedidos seja armazenado em uma
+-- variável de saída (OUT).
 
 
+--=============================================================================================================
+
+-- 4 Adicione um procedimento ao sistema do restaurante. Ele deve
+-- - Receber um parâmetro de entrada e saída (INOUT)
+-- - Na entrada, o parâmetro possui o código de um cliente
+-- - Na saída, o parâmetro deve possuir o número total de pedidos realizados pelo cliente
+
+--=============================================================================================================
+-- 5 Adicione um procedimento ao sistema do restaurante. Ele deve
+-- - Receber um parâmetro VARIADIC contendo nomes de pessoas
+-- - Fazer uma inserção na tabela de clientes para cada nome recebido
+-- - Receber um parâmetro de saída que contém o seguinte texto:
+-- “Os clientes: Pedro, Ana, João etc foram cadastrados”
+-- Evidentemente, o resultado deve conter os nomes que de fato foram enviados por meio do
+-- parâmetro VARIADIC.
+-- INSERT INTO tb_log (nome_log) VALUES ('adiciona_confirma_cadastro');
 
 
+--=============================================================================================================
+-- 6 Para cada procedimento criado, escreva um bloco anônimo que o coloca em execução
 
 
 
